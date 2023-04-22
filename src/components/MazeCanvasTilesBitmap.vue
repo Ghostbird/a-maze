ef93efa2fa4564e061dc437a6a6c02c9e47fba2b
Ghostbird <gijsbertth@gmail.com>
2023-04-22 23:53:14 +0200

    Add player rendering to tile-set mode

--- src/components/MazeCanvasTilesBitmap.vue
+++ src/components/MazeCanvasTilesBitmap.vue
@@ -1,28 +1,43 @@
 <script setup lang="ts">
 import type { Player2D } from '@/utils/player';
 import type { Room2D } from '@/utils/room';
+import type { TileSet2D } from '@/utils/tile';
 import { getBitmapTileSet } from '@/utils/tile';
-import { tap } from 'rxjs';
-import { ref, onMounted } from 'vue';
-
-const canvas = ref(null as HTMLCanvasElement | null);
+import { tap, type Subscription } from 'rxjs';
+import { ref, onMounted, onUnmounted } from 'vue';
+const canvas = ref<HTMLCanvasElement>();
+const tileSet = ref<TileSet2D>()
+let tileSetSubscription: Subscription;
 
 const props = withDefaults(defineProps<{
   tilesUri?: string,
   rooms: Room2D[],
   player: Player2D,
 }>(), { tilesUri: 'tilesets/test/test.png' });
-
-onMounted(() => getBitmapTileSet(props.tilesUri).pipe(
-  tap(({ size, ...tiles }) => {
+onMounted(() => tileSetSubscription = getBitmapTileSet(props.tilesUri).pipe(
+  tap(({ size }) => {
+    // Update canvas size on tile-set load
     canvas.value!.width = size * (Math.max(...props.rooms.map(room => room.x)) + 1)
     canvas.value!.height = size * (Math.max(...props.rooms.map(room => room.y)) + 1)
-    const ctx2d = canvas.value!.getContext('2d');
-    for (const room of props.rooms) {
-      ctx2d?.drawImage(tiles[room.tileType], room.x * size, room.y * size);
-    }
-  })
-).subscribe())
+  }))
+  .subscribe(loadedTileSet => {
+    tileSet.value = loadedTileSet
+    // We know this only happens once, so this is safe.
+    window.requestAnimationFrame(draw);
+  }))
+onUnmounted(() => tileSetSubscription.unsubscribe())
+
+function draw() {
+  // Don't draw until tile-set is loaded.
+  if (!tileSet.value) { return };
+  const { size, ...tiles } = tileSet.value;
+  const ctx2d = canvas.value!.getContext('2d');
+  for (const room of props.rooms) {
+    ctx2d?.drawImage(tiles[room.tileType], room.x * size, room.y * size);
+  }
+  ctx2d?.drawImage(tiles[props.player.direction], props.player.room.x * size, props.player.room.y * size)
+  window.requestAnimationFrame(draw);
+}
 
 </script>
 <template>
