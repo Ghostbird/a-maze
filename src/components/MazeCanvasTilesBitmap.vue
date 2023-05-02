47914381da57119d1ad1543c2cae4b905c2d7840
Ghostbird <gijsbertth@gmail.com>
2023-05-02 18:57:45 +0200

    Do not rerender the maze every frame

--- src/components/MazeCanvasTilesBitmap.vue
+++ src/components/MazeCanvasTilesBitmap.vue
@@ -5,7 +5,8 @@ import type { TileSet2D } from '@/utils/tile';
 import { getBitmapTileSet } from '@/utils/tile';
 import { tap, type Subscription } from 'rxjs';
 import { ref, onMounted, onUnmounted } from 'vue';
-const canvas = ref<HTMLCanvasElement>();
+const mazeCanvas = ref<HTMLCanvasElement>();
+const playerCanvas = ref<HTMLCanvasElement>();
 const tileSet = ref<TileSet2D>()
 let tileSetSubscription: Subscription;
 
@@ -15,16 +16,46 @@ const props = withDefaults(defineProps<{
   player: Player2DAnimation,
 }>(), { tilesUri: 'tilesets/test/test.png' });
 
+const drawMaze = () => {
+  // Abort if tile-set is not loaded or canvas was not rendered
+  if (!tileSet.value || !mazeCanvas.value) { return };
+  if (!tileSet.value) { return };
+  const { size, ...tiles } = tileSet.value;
+  const ctx2d = mazeCanvas.value.getContext('2d');
+  // Erase canvas
+  ctx2d?.clearRect(0, 0, mazeCanvas.value.width, mazeCanvas.value.height)
+  // Draw maze
+  for (const room of props.rooms) {
+    ctx2d?.drawImage(tiles[room.tileType], room.x * size, room.y * size);
+  }
+}
+
+const drawPlayer = (time: DOMHighResTimeStamp) => {
+  // Abort if tile-set is not loaded or canvas was not rendered
+  if (!tileSet.value || !playerCanvas.value) { return };
+  const { size, ...tiles } = tileSet.value;
+  // Update player animation
+  props.player.update(time)
+  const ctx2d = playerCanvas.value.getContext('2d');
+  // Erase canvas
+  ctx2d?.clearRect(0, 0, playerCanvas.value.width, playerCanvas.value.height)
+  // Draw player
+  ctx2d?.drawImage(tiles[props.player.direction], props.player.x * size, props.player.y * size)
+  window.requestAnimationFrame(drawPlayer);
+}
+
 onMounted(() => tileSetSubscription = getBitmapTileSet(props.tilesUri).pipe(
   tap(({ size }) => {
     // Update canvas size on tile-set load
-    canvas.value!.width = size * (Math.max(...props.rooms.map(room => room.x)) + 1)
-    canvas.value!.height = size * (Math.max(...props.rooms.map(room => room.y)) + 1)
+    mazeCanvas.value!.width = playerCanvas.value!.width = size * (Math.max(...props.rooms.map(room => room.x)) + 1)
+    mazeCanvas.value!.height = playerCanvas.value!.height = size * (Math.max(...props.rooms.map(room => room.y)) + 1)
   }))
   .subscribe(loadedTileSet => {
     tileSet.value = loadedTileSet
-    // We know this only happens once, so this is safe.
-    window.requestAnimationFrame(draw);
+    // Draw the maze once
+    drawMaze();
+    // Start the player animation loop once
+    window.requestAnimationFrame(drawPlayer);
   })
 )
 
@@ -34,23 +65,10 @@ onUnmounted(() => {
   tileSetSubscription.unsubscribe();
 })
 
-function draw(time: DOMHighResTimeStamp) {
-  // Don't draw until tile-set is loaded.
-  if (!tileSet.value) { return };
-  const { size, ...tiles } = tileSet.value;
-  const ctx2d = canvas.value!.getContext('2d');
-  // Update player animation
-  props.player.update(time)
-  for (const room of props.rooms) {
-    ctx2d?.drawImage(tiles[room.tileType], room.x * size, room.y * size);
-  }
-  ctx2d?.drawImage(tiles[props.player.direction], props.player.x * size, props.player.y * size)
-  window.requestAnimationFrame(draw);
-}
-
 </script>
 <template>
-  <canvas ref="canvas"></canvas>
+  <canvas ref="mazeCanvas"></canvas>
+  <canvas ref="playerCanvas"></canvas>
 </template>
 <style>
 </style>
